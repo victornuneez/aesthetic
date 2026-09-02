58f15c61bbee5a73cde324d710b310d9171bb769
Victor Nuñez <victornuneezdfgh@gmail.com>
2026-09-02 17:51:18 -0300

    feat: agrega la descripcion del permiso de la camara en el archivo android manifest

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.aesthetic"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
